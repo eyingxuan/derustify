c0a3e54fe81534a49a2025663e955bd467cb43f1
Ying <engyingxuan@gmail.com>
2020-06-08 21:14:29 +0000

    added breakpoint struct

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,18 +1,24 @@
-use anyhow::{Error, Result};
 use anyhow::{Context, Result};
 use nix::{sys::ptrace, unistd::Pid};
+use ptrace::AddressType;
 use rustyline::{error::ReadlineError, Editor};
+use std::ffi::c_void;
+
+mod breakpoint;
+use breakpoint::Breakpoint;
 
 pub struct Debugger {
-    child_pid: Pid,
+    pid: Pid,
     editor: Editor<()>,
+    breakpoints: Vec<Breakpoint>,
 }
 
 impl Debugger {
     pub fn new(pid: Pid) -> Self {
         Debugger {
-            child_pid: pid,
+            pid,
             editor: Editor::<()>::new(),
+            breakpoints: Vec::new(),
         }
     }
 
@@ -35,8 +41,24 @@ impl Debugger {
         }
     }
 
+    pub fn write_addr(&mut self, addr: AddressType, data: u64) -> Result<()> {
+        let casted_data = unsafe { data as *mut c_void };
+        ptrace::write(self.pid, addr, casted_data)
+            .with_context(|| format!("writing {:X?} to {:X?} failed", casted_data, addr))
+    }
+
+    pub fn read_addr(&self, addr: AddressType) -> Result<i64> {
+        ptrace::read(self.pid, addr).with_context(|| format!("reading from {:X?} failed", addr))
+    }
+
     pub fn handle_continue(&mut self) -> Result<()> {
         println!("Continuing debugee execution");
         ptrace::cont(self.pid, None).with_context(|| "could not continue tracing")
+    }
+
+    pub fn handle_add_bp(&mut self, addr: u64) -> Result<()> {
+        let bp = Breakpoint::new(addr, self)?;
+        self.breakpoints.push(bp);
+        Ok(())
     }
 }
---
+++ src/debugger/breakpoint.rs
@@ -0,0 +1,36 @@
+use super::Debugger;
+use anyhow::Result;
+use nix::sys::ptrace::AddressType;
+
+pub struct Breakpoint {
+    addr: AddressType,
+    enabled: bool,
+    instruct_byte: Option<u64>,
+}
+
+impl Breakpoint {
+    pub fn new(addr: u64, debugger: &mut Debugger) -> Result<Self> {
+        let mut bp = Breakpoint {
+            addr: unsafe { addr as AddressType },
+            enabled: false,
+            instruct_byte: None,
+        };
+        bp.set_breakpoint(debugger)?;
+        Ok(bp)
+    }
+
+    pub fn set_breakpoint(&mut self, debugger: &mut Debugger) -> Result<()> {
+        let curr_inst = debugger.read_addr(self.addr)? as u64;
+        self.instruct_byte = Some(curr_inst);
+        debugger.write_addr(self.addr, curr_inst | 0xCC)?;
+        Ok(())
+    }
+
+    pub fn rem_breakpoint(&mut self, debugger: &mut Debugger) -> Result<()> {
+        let inst = self
+            .instruct_byte
+            .expect("instruction must be cached when breakpoint is set");
+        debugger.write_addr(self.addr, inst)?;
+        Ok(())
+    }
+}
